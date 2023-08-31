cf5f8eb74effddb7bda2e313077510fb9f72f780
clebsantos404 <clebsondantas404@gmail.com>
2023-08-31 15:18:53 -0300

    feat: Created the function to erro in empty input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,84 @@ const mes = data.getMonth() + 1;
 const ano = data.getFullYear();
 
 function calcularAno() {
-
   // Inputs
-  const diaValue = parseInt(document.getElementById('dayV').value);
-  const mesValue = parseInt(document.getElementById('monthV').value);
-  const anoValue = parseInt(document.getElementById('yearV').value);
+  const inputDay = document.getElementById("dayV");
+  const inputMonth = document.getElementById("monthV");
+  const inputYear = document.getElementById("yearV");
 
-  if(diaValue <= 31) {
-    document.getElementById('seeDays').innerHTML = `<em>${(dia - diaValue)}</em>`;
+  const diaValue = parseInt(inputDay.value);
+  const mesValue = parseInt(inputMonth.value);
+  const anoValue = parseInt(inputYear.value);
+
+  if (inputDay.value == "" && inputMonth.value == "" && inputYear.value == "") {
+    callbackErro();
+  } else {
+    callbackSucesso();
   }
-  if(mesValue <= 12) {
-    document.getElementById('seeMonth').innerHTML = `<em>${(mes - mesValue)}</em>`;
+
+  function callbackErro() {
+    const alert = document.getElementsByClassName("required");
+    const h3 = document.getElementsByClassName("h3");
+
+    for (var i in h3) {
+      if (i <= h3.length) {
+        let arr2 = [...h3];
+        arr2[i].style.color = "red";
+      }
+    }
+
+    for (var i in alert) {
+      let arr = [...alert];
+      if (i <= alert.length) {
+        arr[i].innerHTML = "This field is empty";
+        arr[i].style.color = "red";
+      }
+    }
+
+    inputDay.style.border = "1px solid red";
+    inputMonth.style.border = "1px solid red";
+    inputYear.style.border = "1px solid red";
   }
-  if(anoValue <= ano) {
-    document.getElementById('seeYear').innerHTML = `<em>${(ano - anoValue)}</em>`;
+
+  function callbackSucesso() {
+    const alert = document.getElementsByClassName("required");
+    const h3 = document.getElementsByClassName("h3");
+
+    inputDay.style.border = "1px solid rgba(0, 0, 0, 0.192)";
+    inputMonth.style.border = "1px solid rgba(0, 0, 0, 0.192)";
+    inputYear.style.border = "1px solid rgba(0, 0, 0, 0.192)";
+
+    for (var i in h3) {
+      if (i <= h3.length) {
+        let arr2 = [...h3];
+        arr2[i].style.color = "#808080";
+      }
+    }
+
+    for (var i in alert) {
+      let arr = [...alert];
+      if (i <= alert.length) {
+        arr[i].innerHTML = "";
+      }
+
+      if (diaValue <= 31) {
+        document.getElementById("seeDays").innerHTML = `<em>${
+          dia - diaValue
+        }</em>`;
+        inputDay.value = "";
+      }
+      if (mesValue <= 12) {
+        document.getElementById("seeMonth").innerHTML = `<em>${
+          mes - mesValue
+        }</em>`;
+        inputMonth.value = "";
+      }
+      if (anoValue <= ano) {
+        document.getElementById("seeYear").innerHTML = `<em>${
+          ano - anoValue
+        }</em>`;
+        inputYear.value = "";
+      }
+    }
   }
 }
